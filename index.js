92387d0b154cc010bb6491f3117a6cae10fa2b7f
Patrice Mocombe <patricemocombe@gmail.com>
2022-03-16 16:29:21 -0400

    commit tree 0bbedc1905f0666d759255b593275cce00d0e50f

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  let result;
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  }
+  if (someValue >= 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (someValue >= 2500) {
+    result = 'No can do.';
+  }
+  return result
   // Write your code here!
+};
+scuberGreetingForFeet(199);
+
+scuberGreetingForFeet(2001);
+
+scuberGreetingForFeet(2501);
+
+
+
+ function ternaryCheckCity(someCity){
+
+  return someCity === 'NYC'? 'Ok, sounds good.' : `No go.`
+  };
+
+ternaryCheckCity('NYC');
+
+ternaryCheckCity('Pittsburgh');
+
+
+function switchOnCharmFromTip(someTip){
+  switch (someTip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  };
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip();
